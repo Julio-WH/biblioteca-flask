98e8e6c484034f05f32bcdcefd5b94c0049c76b7
Julio-WH <julio@wisphub.net>
2023-09-21 15:00:35 -0500

    Actualizacion de docker

--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{os.getenv("DB_URI")}'
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("HOST_DATABASES")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
